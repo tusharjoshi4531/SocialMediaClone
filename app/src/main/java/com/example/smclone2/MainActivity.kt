61fcf9ea165c499342efe2984221ccf7082df32b
Tushar Joshi <tusharjoshi4531@gmail.com>
2021-12-02 19:09:14 +0530

    Opening login activity implemented

--- app/src/main/java/com/example/smclone2/MainActivity.kt
+++ app/src/main/java/com/example/smclone2/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.smclone2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    // Views
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Binding views
+        btnLogin = findViewById(R.id.MainActivity_btn_login)
+        btnSignup = findViewById(R.id.MainActivity_btn_signup)
+
+        // Implementing login button
+        btnLogin.setOnClickListener {
+            val i = Intent(this, LoginActivity::class.java)
+            startActivity(i)
+        }
     }
 }
